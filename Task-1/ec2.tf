4df468202854fae347053cb883fb3c5611f1cb10
MeetP20 <124292920+MeetP20@users.noreply.github.com>
2026-02-05 08:35:02 +0530

    Update ec2.tf

--- Task-1/ec2.tf
+++ Task-1/ec2.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu" {
     owners = ["099720109477"]
 }
 
-resource "aws_instance" "master" {
+resource "aws_instance" "jenkins" {
   ami = data.aws_ami.ubuntu.id
 
   instance_type = "t3.micro"
@@ -27,10 +27,10 @@ resource "aws_instance" "master" {
   tags = { Name = "jenkins" }
 }
 
-resource "aws_instance" "agent" {
+resource "aws_instance" "node-project" {
   ami = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_1
+  subnet_id     = aws_subnet.public_1.id
   key_name      = var.key_name
 
   vpc_security_group_ids = [aws_security_group.jenkins_agent.id]
